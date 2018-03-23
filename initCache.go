0a30127a4dccd6ce7573a923436ecfc1acc0d97d
John Mclaughlin <john_s_mclaughlin@yahoo.com>
2018-03-22 21:38:19 -0400

    coded initCache

--- initCache.go
+++ initCache.go
@@ -2,9 +2,48 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 
-func initCache( initFile string, cacheItem map [int] ) error {
-	
+func initCache( initFile string, cacheItems map [int] ) error {
+	var initLine  [1000]byte
+	var lineLen   int
+	var key       int
+	var ival      int
+	var fval      float64
+	var sval      [1000]byte
+
+	f, err := os.Open(filePath)
+	defer f.Close()
+
+	if err != nil {
+		return(err)
+	}
+
+	for _ ; err == nil; lineLen, err = fmt.Fscanln( f, initLine ) {
+		if ( err != nil ) break;
+		if ( lineLen < 4 ) continue;
+		
+		_, err = Sscanf( initLine, "%c", &c );
+		switch c {
+		    'i', 'd':
+ 			_, err = fmt.Sscanf( initLine, "%c%d%d", &c, &key, &ival );
+			if ( err != nil ) continue;
+			cacheItems[key] = ival;
+
+		    'f':
+			_, err = fmt.Sscanf( initLine, "%c%d%f", &c, &key, &fval );
+			if ( err != nil ) continue;
+			cacheItems[key] = fval;
+
+		    's':
+			_, err = fmt.Sscanf( initLine, "%c%d%s", &c, &key, sval );
+			if ( err != nil ) continue;
+			s = new( [len(sval) + 1]byte )
+			copy( s, sval )
+			cacheItems[key] = *s
+		}
+	}
+	return( err )
 }
